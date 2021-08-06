89f4f16d1334436ce1d7e5effc559b62bc5318ff
Victoria <123vika@gmail.com>
2021-08-06 13:56:33 +0300

    home work 3

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <input type="text" v-model="date" placeholder="date" />
+    <input type="text" v-model="category" placeholder="category" />
+    <input type="text" v-model="value" placeholder="value" />
+    <button @click="onSave">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddPaymentForm",
+  data() {
+    return {
+      date: "",
+      category: "",
+      value: "",
+    };
+  },
+  computed: {
+    getCurrentDate() {
+      const today = new Date();
+      const day = today.getDate();
+      const month = today.getMonth() + 1;
+      const year = today.getFullYear();
+      return `${day}.${month}.${year}`;
+    },
+  },
+  methods: {
+    onSave() {
+      const { value, category } = this;
+      const data = {
+        date: this.date || this.getCurrentDate,
+        category,
+        value,
+      };
+      // console.log('emit: addNewPayment', data);
+      this.$emit("addNewPayment", data);
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+// .form {
+// display: none;
+// }
+</style>
---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="list">
+    <table >
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Category</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr class="item" v-for="(item, idx) in list" :key="idx">
+          <td>{{ idx + 1 }}</td>
+          <td>{{ item.date }}</td>
+          <td>{{ item.category }}</td>
+          <td>{{ item.value }}</td>   
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+</style>
